85c5fc745b51b5222af31cc24f9d87ff2c2ab1e3
Paul Richan <75490402+paulrichan@users.noreply.github.com>
2023-06-08 09:44:17 -0700

    add api routes

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics
+from django.contrib.auth.models import User
+from restaurant.models import Booking, Menu
+from restaurant.serializers import BookingSerializer, MenuSerializer, UserSerializer
+from rest_framework import viewsets
 
 
 # Create your views here.
 def index(request):
     return render(request, "index.html", {})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class UsersView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
